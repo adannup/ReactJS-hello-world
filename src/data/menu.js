8e4fb7bef3c78bfff33bf5e741adc6036deb5698
Ramon Adan <adannup@users.noreply.github.com>
2017-05-26 16:09:17 -0500

    Add Props and PropTypes

---
+++ src/data/menu.js
@@ -0,0 +1,18 @@
+export default [
+	{
+		title: 'home',
+		ulr: '/'
+	},
+	{
+		title: 'Blog',
+		url: '/blog'
+	},
+	{	
+		title: 'About Us',
+		url: '/about'
+	},
+	{
+		title: 'Contact Us',
+		url: '/contact'
+	}
+];	
